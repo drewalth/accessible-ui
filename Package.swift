246ed10ab387e28d22f68594e7029061b33e23bf
drewalth <16584198+drewalth@users.noreply.github.com>
2024-09-27 22:12:49 -0600

    build: swift version + platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,6 +8,8 @@ let package = Package(
   platforms: [
     .iOS(.v14),
     .macOS(.v11),
+    .watchOS(.v7),
+    .tvOS(.v14),
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
